e5d534e56565daf12fc6adc4de30978867a9c361
RuiJ72 <rlagos900@gmail.com>
2022-03-27 18:07:47 -0300

    Exercicio simples com laço de Iteração

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace Aula1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numeroDeVezes = 5;
+
+            for (int i=0; i < numeroDeVezes; i++)
+            {
+              Console.WriteLine($"Bem vindo ao C# e .NET {i}");  
+            }
         }
     }
 }
+
+
+// Exemplo em C# de uma Iteração em um laço de repetição
+// o Write Line é como o console.log do JS
+// int é uma declaração de variável que neste caso recebe o valor 5
